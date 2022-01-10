b5fa46a757c21cfefaa333fccfb08f8228e7dcf0
asmaa <asma.razak@softwareq.com>
2022-01-10 11:40:00 +0800

    State. Structural and attribute directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'team-app';
+  newMember = '';
+  members: string[] = [];
+
+  addMember() {
+    this.members.push(this.newMember);
+    console.log(this.members)
+    this.newMember = '';
+  }
+
+  onInput(member: string) {
+    this.newMember = member;
+    console.log(this.newMember);
+  }
 }
